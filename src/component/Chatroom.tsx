7aa3306a47936d2bf097fa9fda15ee0cf2941b1c
Jeffrey Zalischi <jay.zalischi@gmail.com>
2022-05-21 20:42:23 -0400

    populates last 10 messsages in real time
    created a custom query to populate last 10 messages and then used a react firebase hook to fetch data and populate it in real time

--- src/component/Chatroom.tsx
+++ src/component/Chatroom.tsx
@@ -2,7 +2,6 @@ import { getAuth } from 'firebase/auth';
 //FIREBASE AUTH
 import { useAuthState } from 'react-firebase-hooks/auth';
 //FIREBASE FIRESTORE
-
 import 'firebase/compat/firestore';
 import { initializeApp } from 'firebase/app';
 import {
@@ -20,6 +19,12 @@ import {
   setDoc,
   where,
 } from 'firebase/firestore';
+//REACT FIREBASE HOOKS
+import {
+  useCollection,
+  useCollectionData,
+} from 'react-firebase-hooks/firestore';
+
 import { useEffect } from 'react';
 
 const Chatroom = () => {
@@ -27,80 +32,20 @@ const Chatroom = () => {
   const auth = getAuth();
   const [user] = useAuthState(auth);
 
-  const firestore = getFirestore();
-
-  // ADD DATA TO FIRESTORE EXAMPLE
-  //SPECIFY THE PATH
-  const specialOfTheDay = doc(firestore, 'dailySpecial/2021-09-15');
-  async function writeDailySpecial() {
-    const docData = {
-      description: 'A delicious thing',
-      price: 1.0,
-      milk: 'none',
-      vegan: true,
-    };
-    await setDoc(specialOfTheDay, docData);
-  }
-
-  //ADD A NEW DOCUMENT AND COLLECTION WITHOUT SPECIFYING PATH
-  //this method returns a newDoc variable
-  const ordersCollection = collection(firestore, 'orders');
-
-  async function addNewDocument() {
-    const newDoc = await addDoc(ordersCollection, {
-      customer: 'Brian',
-      drink: 'milk',
-      total_cost: (100 + Math.floor(Math.random() * 400)) / 100,
-    });
-    // console.log(newDoc.path);
-  }
-
-  //Read a single documnent
-  async function readSingleDocument() {
-    const mySnapshot = await getDoc(specialOfTheDay);
-
-    if (mySnapshot.exists()) {
-      const docData = mySnapshot.data();
-      // console.log(`My data in string form is ${docData.vegan}`);
-    }
-  }
-
-  //READ A DOCUMENT IN REAL TIME
-  function listenToDocument() {
-    onSnapshot(specialOfTheDay, (docSnapshot) => {
-      if (docSnapshot.exists()) {
-        const docData = docSnapshot.data();
-        console.log(
-          `In realtime, docData is currently is ${JSON.stringify(docData)}`
-        );
-      }
-    });
-  }
-
-  //CALL QUERY TO GET MULTIPLE DOCS
-  async function queryforDocuments() {
-    const customerOrderQuery = query(
-      collection(firestore, 'orders'),
-      where('drink', '==', 'milk'),
-      // orderBy('total_cost'),
-      limit(10)
-    );
-    const querySnapshot = await getDocs(customerOrderQuery);
-    //allDocs = array
-    const allDocs = querySnapshot.forEach((snap) => {
-      console.log(
-        `Document ${snap.id} contains ${JSON.stringify(snap.data())}`
-      );
-    });
-  }
-
-  useEffect(() => {
-    listenToDocument();
-    writeDailySpecial();
-    // addNewDocument();
-    readSingleDocument();
-    queryforDocuments();
-  }, []);
+  //REACT FIREBASE HOOKS
+  const messagesRef = collection(getFirestore(), 'messages');
+  // console.log(messagesRef);
+  // created custom query to populate messages in order created
+  const customQuery = query(
+    collection(getFirestore(), 'messages'),
+    orderBy('createdAt'),
+    limit(10)
+  );
+  // console.log(customQuery);
+  const [values, loading, error] = useCollectionData(customQuery, {
+    idField: 'id',
+  });
+  console.log(values);
 
   return (
     <div>
@@ -108,7 +53,16 @@ const Chatroom = () => {
         <h2>Welcome</h2>
         <h3>{user?.displayName}</h3>
       </div>
-
+      <div>
+        {error && <strong>Error: {JSON.stringify(error)}</strong>}
+        {loading && <span>Collection: Loading...</span>}
+        {values &&
+          values.map((msg) => (
+            <div>
+              <p>{msg.message}</p>
+            </div>
+          ))}
+      </div>
       <button onClick={() => auth.signOut()}>Sign Out</button>
     </div>
   );
